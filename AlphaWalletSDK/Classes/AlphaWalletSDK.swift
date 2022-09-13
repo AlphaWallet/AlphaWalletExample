9511584631e6f69a5cd22596004f4d2094cb2e8a
Krypto Pank <krypto.pank@gmail.com>
2022-09-13 16:06:11 +0300

    add more types visible for sdk

--- AlphaWalletSDK/Classes/AlphaWalletSDK.swift
+++ AlphaWalletSDK/Classes/AlphaWalletSDK.swift
@@ -2,10 +2,48 @@
 import AlphaWalletAddress
 import AlphaWalletFoundation
 
+/**Keystore**/
 public typealias AlphaWallet = AlphaWalletAddress.AlphaWallet
 public typealias Address = AlphaWalletAddress.AlphaWallet.Address
 public typealias Wallet = AlphaWalletFoundation.Wallet
 public typealias Keystore = AlphaWalletFoundation.Keystore
 public typealias EtherKeystore = AlphaWalletFoundation.EtherKeystore
 public typealias JsonWalletAddressesStore = AlphaWalletFoundation.JsonWalletAddressesStore
+
+/**Configuration**/
 public typealias Constants = AlphaWalletFoundation.Constants
+public typealias Config = AlphaWalletFoundation.Config
+
+/**Tokens**/
+public typealias SessionsProvider = AlphaWalletFoundation.SessionsProvider
+public typealias ImportToken = AlphaWalletFoundation.ImportToken
+public typealias TokensProcessingPipeline = AlphaWalletFoundation.TokensProcessingPipeline
+public typealias MultipleChainsTokensDataStore = AlphaWalletFoundation.MultipleChainsTokensDataStore
+public typealias AlphaWalletTokensService = AlphaWalletFoundation.AlphaWalletTokensService
+
+/**NFT**/
+public typealias NFTProvider = AlphaWalletFoundation.NFTProvider
+public typealias AlphaWalletNFTProvider = AlphaWalletFoundation.AlphaWalletNFTProvider
+
+/**Transactions**/
+public typealias TransactionDataStore = AlphaWalletFoundation.TransactionDataStore
+
+public typealias WalletDataProcessingPipeline = AlphaWalletFoundation.WalletDataProcessingPipeline
+public typealias AssetDefinitionStore = AlphaWalletFoundation.AssetDefinitionStore
+public typealias NonActivityMultiChainEventsDataStore = AlphaWalletFoundation.NonActivityMultiChainEventsDataStore
+
+/**Coin Tickers**/
+public typealias TickerIdsFetcherImpl = AlphaWalletFoundation.TickerIdsFetcherImpl
+public typealias TickerIdsFetcher = AlphaWalletFoundation.TickerIdsFetcher
+public typealias CoinTickersFetcher = AlphaWalletFoundation.CoinTickersFetcher
+public typealias CoinGeckoTickersFetcher = AlphaWalletFoundation.CoinGeckoTickersFetcher
+public typealias CoinTickersStorage = AlphaWalletFoundation.CoinTickersStorage
+public typealias ChartHistoryStorage = AlphaWalletFoundation.ChartHistoryStorage
+public typealias TickerIdsStorage = AlphaWalletFoundation.TickerIdsStorage
+public typealias CoinGeckoNetworkProvider = AlphaWalletFoundation.CoinGeckoNetworkProvider
+public typealias AlphaWalletProviderFactory = AlphaWalletFoundation.AlphaWalletProviderFactory
+public typealias CoinGeckoNetworkProviderType = AlphaWalletFoundation.CoinGeckoNetworkProviderType
+
+/**Types**/
+public typealias RPCServer = AlphaWalletFoundation.RPCServer
+public typealias RealmStore = AlphaWalletFoundation.RealmStore
--- Example/AlphaWalletSDK/ViewController.swift
+++ Example/AlphaWalletSDK/ViewController.swift
@@ -8,17 +8,58 @@
 
 import UIKit
 import AlphaWalletSDK
+import Combine
 
 class ViewController: UIViewController {
+
+    private lazy var tokensPipeline: TokensProcessingPipeline = {
+        let eventsDataStore = NonActivityMultiChainEventsDataStore(store: .storage(for: wallet))
+        let assetDefinitionStore = AssetDefinitionStore()
+        let analytics = AnalyticsService()
+
+        let tickerIdsFetcher: TickerIdsFetcher = TickerIdsFetcherImpl(providers: [])
+        let networkProvider: CoinGeckoNetworkProviderType = CoinGeckoNetworkProvider(provider: AlphaWalletProviderFactory.makeProvider())
+        let coinTickersStorage: CoinTickersStorage & ChartHistoryStorage & TickerIdsStorage = RealmStore.shared
+        let coinTickersFetcher: CoinTickersFetcher = CoinGeckoTickersFetcher(networkProvider: networkProvider, storage: coinTickersStorage, tickerIdsFetcher: tickerIdsFetcher)
+
+        let config = Config()
+        let tokensDataStore = MultipleChainsTokensDataStore(store: .storage(for: wallet), servers: [server])
+        let transactionsStorage: TransactionDataStore = .init(store: .storage(for: wallet))
+        let sessionProvider = SessionsProvider(config: config, analytics: analytics)
+        sessionProvider.start(wallet: wallet)
+
+        let importToken = ImportToken(sessionProvider: sessionProvider, wallet: wallet, tokensDataStore: tokensDataStore, assetDefinitionStore: assetDefinitionStore, analytics: analytics)
+        let nftProvider: NFTProvider = AlphaWalletNFTProvider(analytics: analytics, queue: .global())
+        let tokensService = AlphaWalletTokensService(sessionsProvider: sessionProvider, tokensDataStore: tokensDataStore, analytics: analytics, importToken: importToken, transactionsStorage: transactionsStorage, nftProvider: nftProvider, assetDefinitionStore: assetDefinitionStore)
+
+        let pipeline = WalletDataProcessingPipeline(wallet: wallet, tokensService: tokensService, coinTickersFetcher: coinTickersFetcher, assetDefinitionStore: assetDefinitionStore, eventsDataStore: eventsDataStore)
+        pipeline.start()
+
+        return pipeline
+    }()
+    private let server: RPCServer = .main
+    private let wallet: Wallet = Wallet(address: Constants.nullAddress, origin: .hd)
+    private let analytics = AnalyticsService()
+
+    private lazy var keystore: Keystore = {
+        let store = JsonWalletAddressesStore()
+        let storage = try! KeychainStorage(keyPrefix: "test-app")
+        return EtherKeystore(keychain: storage, walletAddressesStore: store, analytics: analytics)
+    }()
+    private var cancelable: Set<AnyCancellable> = .init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let wallet = Wallet(address: Constants.nullAddress, origin: .hd)
-        let store = JsonWalletAddressesStore()
-        let analytics = AnalyticsService()
-        let storage = try! KeychainStorage(keyPrefix: "test-app")
-        let keystore = EtherKeystore(keychain: storage, walletAddressesStore: store, analytics: analytics)
+        tokensPipeline.start()
+        signMessage()
+
+        tokensPipeline.tokenViewModels.sink { tokens in
+
+        }.store(in: &cancelable)
+    }
+
+    private func signMessage() {
         guard let message = "Hello world".data(using: .utf8) else { return }
 
         switch keystore.signMessage(message, for: wallet.address, prompt: "sing message") {
